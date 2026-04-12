72433ee751e5a8011a5a39114f2926fdacb8d961
KlekotkaOrigen <fudomyosama@hotmail.com>
2026-04-12 14:49:28 -0300

    Finish Form.html & Starting Conexion.php

---
+++ crud/conexion.php
@@ -0,0 +1,21 @@
+#Para conectar con la base de datos, se hace un archivo y luego se invoca
+<?php
+    $host="localhost";
+    $pass="";
+    $user="root"
+    $db="ejemplo"
+    $port="3306"
+    
+    // $host="localhost";
+    // $pass="";
+    // $user="root"
+    // $db="ejemplo"
+
+    $conn = new mysqli($host, $user, $pass, $db); //Conservar oreden de variables
+
+    if($conn ->connect_errno){
+        echo "Fallo la conexion";
+        
+    }
+
+?>
