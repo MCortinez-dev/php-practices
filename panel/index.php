6fb6181419cb690534664ff54b3eeeab9aa6a5d3
KlekotkaOrigen <fudomyosama@hotmail.com>
2026-04-26 12:19:52 -0300

    23 04 Class

--- panel/index.php
+++ panel/index.php
@@ -8,6 +8,21 @@
 <body>
     <h1>CRUD Create read Update Delete</h1>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    
+    <?php
+        require_once("conexion.php");
+        if(isset($_POST["cr"])){
+            $nombre=$_POST["nom"];
+            $contrasenia=$_POST["contra"];
+            $hpas=$_POST;
+
+            $sql= "INSERT INTO datos_usuarios(usuarios, password, hash_password) VALUES (:n_nom, :n_cont,:n_hpas)";
+
+            $resultado=$base->prepare($sql);
+            $resultado->execute(array(":n-nom"=>$nom,":n_cont=>$contrasenia", ":n_hpas=>$hpas"));
+        }
+    ?>
+
         <table width="50%" border="1" align="center">
             <tr>
                 <th>ID</th>
@@ -19,9 +34,8 @@
             </tr>
 
             <?php
-            require_once("conexion.php");
             $registro=$base->query("SELECT * FROM datos_usuarios")->fetchAll(PDO::FETCH_OBJ);
-            foreach($registros as $persona);
+            foreach($registros as $persona):
             ?>
 
             <tr>
@@ -29,10 +43,45 @@
                 <td><?php echo $persona->usuarios?></td>
                 <td><?php echo $persona->password?></td>
                 <td><?php echo $persona->hash_password?></td>
+                <td>
+                    <a href="editar.php?"id=<?php echo $persona->id_usuarios?> & nom=<?php echo $persona->usuarios?>& pas=<?php echo $persona->password?>>
+                        <input type="button" value="Modificar">
+                        <img src="/img/dragon.png" alt="icono modificar" width="30px">
+                    </a>
+                </td>
+                <td>
+                    <a href="borrar.php?"id=id=<?php echo $persona->id_usuarios?>>
+                        <input type="button" value="Borrar">
+                        <img src="/img/dragon.png" alt="Imagen eliminar" width="30px">
+                    </a>
+                </td>
             </tr>
 
-        </table>
+            <?php 
+            endforeach;
+            ?>
 
+            <tr>
+                <td></td>
+                <td>
+                    <input type="text" name="nom" id="">
+                </td>
+                <td>
+                    <input type="text" name="contra" id="">
+                </td>
+                <td>
+                    <input type="text" name="phash" id="">
+                </td>
+                <td>
+                <input type="submit" value="Guardar" name="cr">
+                <img src="/img/dragon.png" alt="Imagen guardar" width="30px">
+                <button type="submit" name="insertar" value="Insertar">
+                    Insertar
+                    <img src="/img/dragon.png" alt="Imagen guardar" width="30px">
+                </button>
+                </td>
+            </tr>
+        </table>
     </form>
 
     
